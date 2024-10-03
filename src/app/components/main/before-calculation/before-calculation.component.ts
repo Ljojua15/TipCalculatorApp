5fc4f0e67c39a5f5c1fe2f44f19bce2303859356
Ljojua15 <jojualasha15@gmail.com>
2024-10-03 15:33:16 +0400

    feat:/left-side

--- src/app/components/main/before-calculation/before-calculation.component.ts
+++ src/app/components/main/before-calculation/before-calculation.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { tipsPercent } from './tips';
 import { CommonModule } from '@angular/common';
+import { log } from 'console';
+import { AnySoaRecord } from 'dns';
 
 @Component({
   selector: 'app-before-calculation',
@@ -11,8 +13,33 @@ import { CommonModule } from '@angular/common';
 })
 export class BeforeCalculationComponent {
   public readonly tipsPercent = tipsPercent;
+  public bill: number = 100;
+  public people: number = 5;
+  public percent: any = 0.5;
+  public totalAmountPerPerson: number = 0;
+  public tipAmountPerPerson: number = 0;
 
   constructor() {
-    console.log(this.tipsPercent);
+    this.totoalAmount();
+    this.tipAmount();
+  }
+
+  takeValue(index: number): number {
+    this.percent = this.tipsPercent[index].value;
+    console.log(this.percent);
+    return this.percent;
+  }
+
+  totoalAmount(): number {
+    this.totalAmountPerPerson =
+      (this.bill + this.bill * this.percent) / this.people;
+    console.log(this.totalAmountPerPerson);
+    return this.totalAmountPerPerson;
+  }
+
+  tipAmount(): number {
+    this.tipAmountPerPerson = (this.bill * this.percent) / this.people;
+    console.log(this.tipAmountPerPerson);
+    return this.tipAmountPerPerson;
   }
 }
