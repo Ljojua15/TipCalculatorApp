f4fe00d04e2a5b5b692e815e48d500ba395c588f
Ljojua15 <jojualasha15@gmail.com>
2024-10-04 20:12:12 +0400

    firstInput

--- src/app/components/main/before-calculation/child-calculated/child-calculated.component.ts
+++ src/app/components/main/before-calculation/child-calculated/child-calculated.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child-calculated',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './child-calculated.component.scss'
 })
 export class ChildCalculatedComponent {
-  calculateAllBillS: number = 0
+  @Input('test') test: number = 0
+  @Input('calculateAllTipS') calculateAllTipS: number = 0
 }
